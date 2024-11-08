886bb4fc05eda7739deb0a6548f312856b399081
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-08 09:10:45 +0800

    feat: springboot 集成 opencc4j

---
+++ zero-opencc4j-boot/src/main/java/com/zero/opencc4j/ZeroOpencc4jBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.opencc4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroOpencc4jBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroOpencc4jBootApplication.class, args);
+    }
+
+}
---
+++ zero-opencc4j-boot/src/test/java/com/zero/opencc4j/ZeroOpencc4jBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.opencc4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroOpencc4jBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
